56bb1b2127c9122912730170f55828ac6ef76038
dangiz <dangizzz97@gmail.com>
2018-02-17 15:04:43 +0400

    Базовые фичи и веселье

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Round r=RoundLoader.LoadFromFile("Round.txt");
+            int a = 0;
+        }
+    }
+}
---
+++ Task_1/Round.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task_1
+{
+    public class Round
+    {
+        private double r;
+        
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double R
+        {
+            get
+            {
+                return r;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new FormatException("Attempting assigment a negative radius");
+                }
+                    
+                r = value;
+            }
+        }
+
+        public double Length
+        {
+            get { return 2 * r * Math.PI; }
+        }
+
+        public double Space
+        {
+            get { return Math.Pow(r, 2) * Math.PI; }
+        }
+
+        public Round(double radius, double x, double y)
+        {
+            R = radius;
+            X = x;
+            Y = y;
+        }
+        
+        
+    }
+}
