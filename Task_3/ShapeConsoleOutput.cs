1b1911c89248dccf979fb5512d139a30bd0e4e94
dangiz <dangizzz97@gmail.com>
2018-03-17 15:36:46 +0400

    Task_2 & 3

---
+++ Task_3/ShapeConsoleOutput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3.Shapes
+{
+    class ShapeConsoleOutput:IShapeOutput
+    {
+       public void Draw(IEnumerable<IShape> shapes)
+        {
+            foreach(IShape shape in shapes)
+            {
+                Console.WriteLine(shape.GetDescription());
+            }
+        }
+
+        public void Draw(IShape shape)
+        {
+            Console.Write(shape);
+        }
+        
+
+    }
+}
