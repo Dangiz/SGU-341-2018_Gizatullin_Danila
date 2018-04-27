ed7d098d095a1581be1dfbb8fca929cc96917153
dangiz <dangizzz97@gmail.com>
2018-04-27 18:25:31 +0400

    Got one character implement
    Just for example

---
+++ Task_3/Game/Models/PlayerCharacter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3.Game.Models
+{
+    class PlayerCharacter : GameCharacter
+    {
+        public GridTransform Transform { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+
+        public void Destroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnBorderCrossing(int Height, int Width)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCollision(GameObject obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            //Input & Transform logic
+            throw new NotImplementedException();
+        }
+    }
+}
